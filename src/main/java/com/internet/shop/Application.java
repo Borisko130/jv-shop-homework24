6e950b1ca171f0b9bc937ca0bbee71f0d3ce6d81
Borisko130 <krimenyukb@gmail.com>
2020-09-03 14:46:24 +0300

    Renamed Main class to Application

--- src/main/java/com/internet/shop/Application.java
+++ src/main/java/com/internet/shop/Application.java
@@ -3,7 +3,7 @@ package com.internet.shop;
 import com.internet.shop.model.Product;
 import com.internet.shop.storage.Storage;
 
-public class Main {
+public class Application {
     public static void main(String[] args) {
         Storage.addProduct(new Product("Hat", 15.0));
         Storage.addProduct(new Product("Banana", 2.0));
